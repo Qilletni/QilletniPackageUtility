aab843deb1c088c5912e7c0a5ce818c28800c876
Adam Yarris <adamyarris@gmail.com>
2026-01-11 15:02:55 -0500

    Removed qilletni-api dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,7 @@
 module qilletni.pkgutil {
-    requires qilletni.api;
     requires com.google.gson;
     requires org.yaml.snakeyaml;
 
-    exports dev.qilletni.pkgutil;
-    exports dev.qilletni.pkgutil.adapters;
     exports dev.qilletni.pkgutil.manifest;
     exports dev.qilletni.pkgutil.manifest.models;
 }
